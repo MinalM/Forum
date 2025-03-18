5c3df62bff1cdb23953ebb0554dd2a9e42b4ef65
MinalM <minal.mishra@gmail.com>
2025-03-17 21:35:55 -0700

    lint error part 2

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { useAuth } from '../context/AuthContext';
 import { useAlert } from '../context/AlertContext';
 
 const CreatePost = () => {
